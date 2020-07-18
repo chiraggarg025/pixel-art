3a70092b85a14f4d1cfcd69d2f2b20da0c50ebd5
chanakya.garg <chiraggarg025@gmail.com>
2020-07-18 18:15:45 +0530

    Complete functionality

--- script.js
+++ script.js
@@ -4,26 +4,35 @@ var color = 'black';
 document.getElementById('clear').addEventListener('click',function(){
     location.reload();
 })
+
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 1; c <= (rows * cols); c++) {
     let cell = document.createElement("div");
-    /* console.log() */
+    
+    
     
     cell.setAttribute('id',c);
     if(c>(rows*cols)-Math.max(rows,cols)){
-    	cell.style.backgroundColor=getRandomColor();
+        cell.style.backgroundColor=getRandomColor();
+        
       cell.addEventListener('click',function catchColor(){
         let div = document.getElementById(this.id);
         color = div.style.backgroundColor;
       });
     }else{
     	cell.addEventListener('click',function changeColor(event){
-					let div =document.getElementById(this.id);
+		  let div =document.getElementById(this.id);
           div.style.backgroundColor=color;
-    	});
+        });
     	
+    }
+    container.ondragover=function(e){
+        e.preventDefault();  
+        let id = e.target.id;
+        if(id<(rows*cols)-Math.max(rows,cols))
+        document.getElementById(id).style.backgroundColor=color;
     }
     
     
